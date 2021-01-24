437c9329e3cc60eca90d56e48aa3055114897cb4
Sim kaur <moblue2012@gmail.com>
2021-01-23 20:18:02 -0500

    added routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const path = require("path");
+
+// "/notes" responds with the notes.html file
+router.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+  
+  // All other routes respond with the index.html file
+  router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+  
+  module.exports = router;
